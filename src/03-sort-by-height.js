53cb79525e830594977293a92193a639a69468ff
Andrey Serikov <as.korag@protonmail.com>
2021-04-10 01:51:56 +0300

    feat: solve sort-by-height task

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -10,7 +10,19 @@
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
+  const heights = arr.filter((num) => num !== -1).sort((a, b) => a - b);
+  const result = arr.map((num) => (num === -1 ? -1 : 0));
 
+  let pos = 0;
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === 0) {
+      result[i] = heights[pos];
+      pos++;
+    }
+  }
+
+  return result;
 }
 
 module.exports = sortByHeight;
