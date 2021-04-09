572ab92d2c5f6918c2ce141896d47536814a4da8
Andrey Serikov <AS.Korag@protonmail.com>
2021-04-09 15:20:33 +0300

    init: start delete-digit task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -2,15 +2,14 @@
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
  *
- * @param {Number} n
+ * @param {Number} num
  * @return {Number}
  *
  * @example
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(num) {
 }
 
 module.exports = deleteDigit;
