c7b236c27a6144a167c9264b8049d5ba461d815d
Andrey Serikov <AS.Korag@protonmail.com>
2021-04-09 15:07:45 +0300

    feat: solve encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,7 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
+function getCode(str) {
+  const char = str[0];
+  const amount = str.length;
+  return `${amount === 1 ? '' : amount}${char}`;
+}
+
 function encodeLine(str) {
+  let pos = 0;
+  const result = [];
+
+  while (pos < str.length) {
+    const char = str[pos];
+    const regExp = new RegExp(`${char}+`, 'g');
+    regExp.lastIndex = pos;
+
+    const match = regExp.exec(str)[0];
+
+    result.push(getCode(match));
+    
+    pos = regExp.lastIndex;
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
