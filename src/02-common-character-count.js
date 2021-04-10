c5e3851d280f49f7861553efe505ec02935034c1
Andrey Serikov <as.korag@protonmail.com>
2021-04-10 19:01:30 +0300

    feat: solve common-character-count task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -10,7 +10,20 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
+  const set = new Set(s1);
 
+  let result = 0;
+
+  set.forEach((char) => {
+    const regExp = new RegExp(`${char}`, 'g');
+
+    const match1 = s1.match(regExp) || [];
+    const match2 = s2.match(regExp) || [];
+
+    result += Math.min(match1.length, match2.length);
+  });
+
+  return result;
 }
 
 module.exports = getCommonCharacterCount;
